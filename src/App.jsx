bed76acd1e643e4956349874c2e253f7868b82ee
ENassef <nassef_i@yahoo.com>
2025-09-23 22:09:30 +0300

    feat: initial project setup
    - Add base React application with Vite
    - Configure Tailwind CSS with custom theme
    - Set up routing structure (Home, About, Services, Portfolio, Blogs, Contact)
    - Implement AOS animations
    - Add Font Awesome icons
    - Include basic component structure

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Service from "./Pages/Service/Service";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Blogs from "./Pages/Blogs/Blogs";
+import Contact from "./Pages/Contact/Contact";
+import Error from "./Pages/Error/Error";
+
+function App() {
+    useEffect(() => {
+        AOS.init({
+            duration: 500,
+            easing: "ease-in-out",
+            once: false,
+            mirror: true,
+        });
+    }, []);
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    index: true,
+                    element: <Home />,
+                },
+                {
+                    path: "home",
+                    element: <Home />,
+                },
+                {
+                    path: "about",
+                    element: <About />,
+                },
+                {
+                    path: "service",
+                    element: <Service />,
+                },
+                {
+                    path: "portfolio",
+                    element: <Portfolio />,
+                },
+                {
+                    path: "blogs",
+                    element: <Blogs />,
+                },
+                {
+                    path: "contact",
+                    element: <Contact />,
+                },
+                {
+                    path: "*",
+                    element: <Error />,
+                },
+            ],
+        },
+    ]);
+
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+}
+
+export default App;
