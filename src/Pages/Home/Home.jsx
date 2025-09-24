50a857edbd7f2a283a3d995820e5804d132d5f93
ENassef <nassef_i@yahoo.com>
2025-09-25 02:15:46 +0300

    fixing the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Hero from '../../Components/Hero/Hero'
-import OurProcess from '../../Components/OurProcess/OurProcess'
-import Testimonials from '../../Components/Testimonials/Testimonials'
+import React from "react";
+import Hero from "../../Components/Hero/Hero";
+import OurProcess from "../../Components/OurProcess/OurProcess";
+import Testimonials from "../../Components/Testimonials/Testimonials";
 
-export default function
-() {
-  return (
-    <>
-        <Hero/>
-        <OurProcess/>
-        <Testimonials/>
-    </>
-  )
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <OurProcess />
+            <Testimonials />
+        </>
+    );
 }
