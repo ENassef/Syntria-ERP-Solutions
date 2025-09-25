c8cbc2b746ba0ecd58d1ccc1c875c280e6e5519a
ENassef <nassef_i@yahoo.com>
2025-09-25 14:17:29 +0300

    Fixing the css and Container

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import PortfolioCom from "../../Components/Portfolio/PortfolioCom"
-import Footer from '../../Components/Footer/Footer'
+import React from "react";
+import PortfolioCom from "../../Components/Portfolio/PortfolioCom";
+import Footer from "../../Components/Footer/Footer";
 
 export default function Portfolio() {
-  return (
-    <>
-      <PortfolioCom/>
-      <Footer/>
-    </>
-  )
+    return (
+        <>
+            <PortfolioCom />
+        </>
+    );
 }
