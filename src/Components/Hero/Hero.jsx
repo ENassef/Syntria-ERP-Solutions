328d6df71200927335bf187cea7e31208e6acc41
mohamedsedkii <mosedkii@gmail.com>
2025-09-27 22:20:59 +0300

    Editing Active link and adding cursor hover for the text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import HeroImg from "../../assets/Hero.png";
 
 export default function Hero() {
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../Navbar/Navbar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
-    <div className='container'>
-    <Navbar/>
-    <Outlet/>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
